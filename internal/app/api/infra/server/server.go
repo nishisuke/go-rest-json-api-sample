50c98e1f16ff0575d535c23b052384f3dabb7a53
nishisuke <ryoskns+git@gmail.com>
2022-04-30 16:58:37 +0900

    vali

--- internal/app/api/infra/server/server.go
+++ internal/app/api/infra/server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"your_module/internal/app/api/infra/env"
-	"your_module/internal/app/api/validation"
 	"your_module/internal/pkg/server"
 
 	"github.com/labstack/echo/v4"
@@ -11,13 +10,13 @@ import (
 	// "github.com/nishisuke/echo-go1.18/internal/infra/db"
 )
 
-func Start(logger echo.Logger, en env.Env) error {
+func Start(logger echo.Logger, validator echo.Validator, en env.Env) error {
 	e := echo.New()
 	e.Logger = logger
 
 	e.HideBanner = !en.IsLocal()
 
-	e.Validator = validation.NewValidation()
+	e.Validator = validator
 
 	//e.HTTPErrorHandler = customHTTPErrorHandler
 	// middlewares(e)
--- internal/app/api/start.go
+++ internal/app/api/start.go
@@ -3,6 +3,7 @@ package api
 import (
 	"your_module/internal/app/api/infra/env"
 	"your_module/internal/app/api/infra/server"
+	"your_module/internal/app/api/validation"
 	"your_module/internal/pkg/db"
 
 	"github.com/labstack/echo/v4"
@@ -18,5 +19,5 @@ func Start(logger echo.Logger, e env.Env) error {
 	}
 	gormDB = tmp
 
-	return server.Start(logger, e)
+	return server.Start(logger, validation.NewValidation(), e)
 }
