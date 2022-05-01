90706305f015b318f482b99b93ace81300f2fb9f
nishisuke <ryoskns+git@gmail.com>
2022-05-01 14:19:58 +0900

    f

--- scripts/openapi.sh
+++ scripts/openapi.sh
@@ -2,7 +2,7 @@
 
 rm -rf internal/pkg/openapi
 
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i local/api/swagger.yml -g go -o /local/internal/pkg/openapi --package-name openapi
+docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i local/api/swagger.yml -g go-server -o /local/internal/pkg/openapi --package-name openapi
 
 rm -f internal/pkg/openapi/go.mod
 rm -f internal/pkg/openapi/go.sum
@@ -12,5 +12,7 @@ rm -f internal/pkg/openapi/git_push.sh
 rm -f internal/pkg/openapi/.gitignore
 rm -f internal/pkg/openapi/.travis.yml
 rm -f internal/pkg/openapi/.openapi-generator/FILES
+rm -f internal/pkg/openapi/Dockerfile
+rm -f internal/pkg/openapi/main.go
 
 docker-compose exec api go fmt ./...
