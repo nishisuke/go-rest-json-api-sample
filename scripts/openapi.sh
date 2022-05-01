339107fd778930fc76e7cb3324b8932109560e27
nishisuke <ryoskns+git@gmail.com>
2022-05-01 11:09:25 +0900

    fix op

--- scripts/openapi.sh
+++ scripts/openapi.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 rm -rf internal/pkg/openapi
+
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i local/api/swagger.yml -g go -o /local/internal/pkg/openapi --package-name openapi
+
 rm -f internal/pkg/openapi/go.mod
 rm -f internal/pkg/openapi/go.sum
+rm -rf internal/pkg/openapi/docs
+rm -rf internal/pkg/openapi/api
+rm -f internal/pkg/openapi/git_push.sh
+rm -f internal/pkg/openapi/.gitignore
+rm -f internal/pkg/openapi/.travis.yml
+rm -f internal/pkg/openapi/.openapi-generator/FILES
